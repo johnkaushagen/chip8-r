2ba49370041c77d6b731213ca775bab3115cdb73
John Kaushagen <johnkaushagen@gmail.com>
2026-01-17 02:16:16 -0500

    Add initial ram implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+const RAM_SIZE: usize = 4096;
+
+struct Emu {
+    memory: [u8; RAM_SIZE],
+}
+
 fn main() {
-    println!("Hello, world!");
+    let chip8: Emu = Emu {
+        memory: [0; RAM_SIZE],
+    };
+    println!("Initialized emulator with {} bytes of RAM.", chip8.memory.len());
 }
