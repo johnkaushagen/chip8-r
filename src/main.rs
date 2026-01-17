6a6269598de8d2846abff0ca8bc0eb9c1048981d
John Kaushagen <johnkaushagen@gmail.com>
2026-01-17 02:25:42 -0500

    Add test for memory initialization

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,18 @@ fn main() {
     };
     println!("Initialized emulator with {} bytes of RAM.", chip8.memory.len());
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_memory_initialization() {
+        let chip8 = Emu {
+            memory: [0; RAM_SIZE],
+        };
+        for &byte in chip8.memory.iter() {
+            assert_eq!(byte, 0);
+        }
+    }
+}
